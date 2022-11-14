e934e51f08277d4864e74a4917733919474304dd
asim59 <simt.ashim@gmail.com>
2022-11-14 21:37:21 +0600

    setup admin panel
    1. Required form validation

---
+++ admin/slider_add.php
@@ -0,0 +1,115 @@
+<?php require_once('header.php'); ?>
+
+<?php
+if (isset($_POST['add_slider_form'])) {
+
+    // Requeired input field validation
+    $valid = 1;
+    $path = $_FILES['slider_photo']['name'];
+    $path_tmp = $_FILES['slider_photo']['tmp_name'];
+
+    // Check error
+    if (empty($path)) {
+        $valid = 0;
+        $error_message = 'You must have to slect a photo';
+    } else {
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $path_tmp);
+        if ($mime != 'image/jpeg' && $mime != 'image/png' && $mime != 'image/gif') {
+            $valid = 0;
+            $error_message = 'Only jpg, png and gif file are allowed for slider photo <br>';
+        }
+    }
+
+    // Success Message
+    if ($valid == 1) {
+        $success_message = 'Successfull';
+    }
+}
+?>
+
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">Add Slider</h1>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-lg-12">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Slider Form Elements
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    <div class="col-lg-12">
+
+
+                        <?php
+                        if (isset($error_message)) {
+                            echo '<div class="alert alert-danger">' . $error_message . '</div>';
+                        }
+                        if (isset($success_message)) {
+                            echo '<div class="alert alert-success">' . $success_message . '</div>';
+                        }
+                        ?>
+
+
+                        <form class="form-horizontal" action="" method="post" enctype="multipart/form-data">
+                            <!-- Slider Title -->
+                            <div class="form-group">
+                                <label for="slider_title" class="col-sm-2 control-label">Slider Title</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="slider_title" name="slider_title">
+                                </div>
+                            </div>
+                            <!-- Slider Sub Title -->
+                            <div class="form-group">
+                                <label for="slider_subtitle" class="col-sm-2 control-label">Slider Sub Title</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="slider_subtitle" name="slider_subtitle">
+                                </div>
+                            </div>
+                            <!-- Slider Button Text -->
+                            <div class="form-group">
+                                <label for="slider_button_text" class="col-sm-2 control-label">Slider Button Text</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="slider_button_text" name="slider_button_text">
+                                </div>
+                            </div>
+                            <!-- Slider Button URL -->
+                            <div class="form-group">
+                                <label for="slider_button_url" class="col-sm-2 control-label">Slider Button URL</label>
+                                <div class="col-sm-10">
+                                    <input type="text" class="form-control" id="slider_button_url" name="slider_button_url">
+                                </div>
+                            </div>
+                            <!-- Slider Photo -->
+                            <div class="form-group">
+                                <label for="slider_photo" class="col-sm-2 control-label">Slider Photo *</label>
+                                <div class="col-sm-10" style="padding-top: 5px;">
+                                    <input type="file" name="slider_photo" id="slider_photo">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-2 col-sm-10">
+                                    <button type="submit" class="btn btn-primary" name="add_slider_form">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <!-- /.row (nested) -->
+            </div>
+            <!-- /.panel-body -->
+        </div>
+        <!-- /.panel -->
+    </div>
+    <!-- /.col-lg-12 -->
+</div>
+<!-- /.row -->
+</div>
+<!-- /#page-wrapper -->
+
+<?php require_once('footer.php'); ?>
